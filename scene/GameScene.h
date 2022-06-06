71e6839d8657861eb1d7e99b2f3aae4e9e47c8f8
KazumaTasiro <k021g1450@g.neec.ac.jp>
2022-06-06 11:22:10 +0900

    変な関数消した

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -74,16 +74,5 @@ class GameScene {
 	/// 
 	//デバックカメラ
 	DebugCamera* debugCamera_ = nullptr;
-
-	int clampCopyProducts(int value, int min, int max)
-	{
-		if (value < min) {
-			return min;
-		}
-		else if (value > max) {
-			return max;
-		}
-		return value;
-	}
 };
 
